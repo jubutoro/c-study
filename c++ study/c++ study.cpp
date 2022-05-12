3f8634210bb38501428b044a1fc3ec5fd652234c
jubutoro <hkhh2512@gmail.com>
2022-05-13 00:06:16 +0900

    operator overloading practice

--- c++ study/c++ study.cpp
+++ c++ study/c++ study.cpp
@@ -8,69 +8,105 @@ using std::cin;
 using std::endl;
 using std::string;
 
-
-class cat {
-	int age;
-	string name;
+ //예시 1
+class point {
+	int x;
+	int y;
 public:
-	cat(int age, string name)
-	{
-		this->age = age;
-		this->name = name;
-		cout << "냥-하\n";
+	point (int i, int j) { x = i; y = j; }
+	int getx() { return x; }
+	int gety() { return y; }
+	void upprint() {
+		++x; ++y;
+		cout << x << " " << y << endl;
 	}
-	cat()
-	{
-		age = 1;
-		name = "고양이";
-	};
-	~cat()
-	{
-		cout << "냥-바\n";
-	};
-	void setAge(int age)
-	{
-		this->age = age;
-	};
-	void setName(string name)
-	{
-		this->name = name;
-	};
-	int getAge()
-	{
-		return age;
-	};
-	string getName()
-	{
-		return name;
-	};
 
+	point up() { // 함수의 형식이 클래스 명이면 객체의 값을 리턴하는 것!
+		++x;
+		++y;
+		return *this;
+	}
 };
 
 
-
-int main(void)
+int main() 
 {
-	cat dobi(5, "dobi"), * pdobi = &dobi, mari(3, "mari");
-	int j, a;
-	string n;
-
-
-	cout << "동적 메모리에 입력할 고양이의 수는 몇입니까? - \n";
-	cin >> j;
-
-	cat* pcat = new cat[j]; //객체 배열을 동적 메모리 할당
-	if (!pdobi) { cout << "동적 메모리 할당이 되지 않았습니다."; exit(1); }
-
-	for (int i = 0; i < j; i++)
-	{
-		cout << i + 1 << "번째 고양이의 나이를 입력해 주세요. : ";
-		cin >> a;
-		cout << i + 1 << "번째 고양이의 나이는 " << a << "입니다.\n";
-		cout << i + 1 << "번째 고양이의 이름을 입력해 주세요. : ";
-		cin >> n;
-		cout << i + 1 << "번째 고양이의 이름은 " << n << "입니다.\n";
-	}
-	return 0;
+	point p1(3, 4);
+	p1.upprint();
+	p1.up();
+	cout << p1.getx() << "..";
+	cout << p1.gety();
 }
+
+	//예시2 [전치연산자]
+point up() {
+	++x;
+	++y;
+	return *this;
+	
+	//이 둘은 같은 효과.
+
+	point poerator++() {//poerator++를 함수 이름으로 생각하면 이해가 쉽다.
+		++x;
+		++y;
+		return *this;
+	
+	}
+	main함수에 ++p1;하면 됨. (마치 ++i처럼 사용)
+
+
+	//예시3 [후치연산자]
+	point poerator--()(int) {//poerator++를 함수 이름으로 생각하면 이해가 쉽다. (int)가 뒤에 있으면 후치!
+		--x;
+		--y;
+		return *this;
+
+	}
+
+
+
+	//예시4 이항연산자 곱셈, 뺄셈 다 가능
+	class Point {
+		int x;
+		int y;
+	public:
+		Point(int i, int j) { x = i; y = j; }
+		Point() { x = 0; y = 0; }
+		int getX() { return x; }
+		int getY() { return y; }
+		Point operator +(Point ob) { //왼쪽 항(주체)를 this포인터로 값을 불러오고, 오른쪽 항(객체)만 가져와서 연산
+			Point temp;
+			temp.x = x + ob.x;  //temp.x = this->x + ob.x;
+			temp.y = y + ob.y;  //temp.y = this->y + ob.x;
+			return temp;
+		}
+	};
+
+	int main() {
+
+		Point ob1(3, 5), ob2(4, 6), ob3;
+		cout << ob3.getX() << "," << ob3.getY() << endl;
+		ob3 = ob1 + ob2;
+		cout << ob3.getX() << "," << ob3.getY();
+	}
+
+	//예시5 == 이항연산자 || 도 가능!
+	class Point {
+		int x;
+		int y;
+	public:
+		Point(int i, int j) { x = i; y = j; }
+		Point() { x = 0; y = 0; }
+		int getX() { return x; }
+		int getY() { return y; }
+		char operator ==(Point ob) { //클래스가 아닌 char (리턴값이 char) 문자열을 쓰고 싶으면 string형으로 바꿔도 됨!
+			return (x == ob.x && y == ob.y) ? 'T' : 'F'; //같으면 T, 다르면 F
+		}
+	};
+
+	int main() {
+
+		Point ob1(3, 5), ob2(4, 6), ob3;
+		cout << (ob1 == ob2); //연산자 우선순위 주의
+	}
 
