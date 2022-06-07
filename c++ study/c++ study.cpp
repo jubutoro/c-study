3ff46429ed2637c8342bbe9481db77f624fc8ed5
jubutoro <hkhh2512@gmail.com>
2022-06-07 16:19:09 +0900

    virtual funtion learning

--- c++ study/c++ study.cpp
+++ c++ study/c++ study.cpp
@@ -8,357 +8,138 @@ using std::cin;
 using std::endl;
 using std::string;
 
-////에시1
-//class A
-//{
-//private://절대 상속 안됨.
-//	void a1() { cout << "a1\n"; }
-//	void a2() { cout << "a2\n"; }
-//public:
-//	void b1() { cout << "a1\n"; }
-//	void b2() { cout << "b2\n"; }
-//	void b3() { cout << "b3\n"; }
-//	void b4() { cout << "b4\n"; }
-//};
-//class B : public A{};
+////overloading - 함수중첩, 같은 클래스 안에서 일어나는 병렬적인 것
+////overriding - 가상함수, 기본 클래스의 멤버함수와 같은 이름의 함수를 파생 클래스에서 재정의하여 사용할 때,
+//// 기본 클래스의 함수에 virtual을 붙여 사용하는 수직적인 것
+//// static binding - 컴파일시 변수의 위치와 함수가 실행할 명령이 결정되는 경우 : static 변수, overloading
+//// dynamic binding - 실제 실행할 떄(run time) 결정되는 경우 : 지역 변수, overrriding
 //
+//void sub();
 //int main() {
-//	B bb;
-//	bb;
-//	return 0;
+//	cout << "start\n";
+//	sub();
+//	sub();
+//	sub();
+//}
+//void sub()
+//{
+//	int x = 10;
+//	//동적 바인딩, run-time시
+//	static int y = 10;
+//	//정적 바인딩, y의 초기값은 컴파일시 10으로 정해지며, 실행시에 이 선언문은 실행하지 않음
+//	//순서는 x가 먼저 이지만, 컴파일시 y의 값이 10으로 정해지므로(static이기 때문) x보다 먼저 값이 결정됨.
+//	cout << x << y << endl;
+//	x++;
+//	y++;
 //}
 //
-////예시2
-//class A
-//{
-//	int x;
-//public:
-//	void setX(int i) { x = i;}
-//	void showX() { cout << x << endl; }
-//
-//};
-//class B :public A
-//{
-//	int y;
-//public:
-//	void setY(int i) { y = i; }
-//	void showY() { cout << y << endl; }
-//
-//};
-//
+////예시 - static_cast
+////dynamic_cast는 down casting을 안전하게 하기 위해 씀 (고급과정)
 //int main() {
-//	B bb;       //파생클래스의 객체
-//	bb.x = 1;   //bb.setX(1)가 맞다
-//	bb.y = 2;   //bb.setY(2)가 맞다
-//	bb.showX(); //기본클래스의 멤버접근
-//	bb.showY(); //파생클래스의 멤버접근
+//	int x = 10, y = 4;
+//	cout << x / y << endl;
+//	cout << (double)x / y << endl;
+//	cout << static_cast<double>(x) / y << endl;
 //}
 //
-////예시 In-class member initializers 근데 되도록 생성자를 이용하길 권장
+//// 예시 - virtual function
+//// overriding을 구현하는 방법
 //
 //class A
 //{
-//	int x = 1;
 //public:
-//	A():x(2){} // A(){x=2;}
-//	void setX(int i) { x = i; }
-//	int getX() { return x; }
+//	virtual int SS(int i) { return(i * 2); }
+//	// 재정의 가능한 함수, 자식 클래스에서 재정의하면 이 함수는 무시됨
+//	// 그런데 함수명, 리턴형, 매개변수의 개수와 자료형은 같아야 하고 기능만 다르게 만들어야 함.
+//
 //};
 //
-//int main()
+//class B : public A
 //{
-//	A a1; //디폴트 생성자 호출, 눈에 안보이는 A(){}
-//	cout << a1.getX() << endl;
-//}
-//
-////예시 private 상속
+//public:
+//	int SS(int i) { return(i * 3); }
+//	//재정의 시에는 별도로 지시어 필요 X
+//};
+
+// static binding(overloading)
+
 //class A
 //{
-//	int x;
 //public:
-//	void setX(int i) { x = i; }
-//	void showX() { cout << x << endl; }
+//	int ss(int i) { return(i * 2); } //앞에 virtual을 붙이면 자식클래스에서 재정의 할 경우 무시됨.
+//	int dd(int i) { return(ss(i)*2); }
+//
 //};
-//class B :private A //private 상속
+//
+//class B : public A
 //{
-//	int y;
 //public:
-//	void setXY(int i, int j) { setX(i); y = j; } //기본 클래스의 public 멤버 접근
-//	void showXY() { showX(); cout << y << endl; }
+//	int ss(int i) { return(i * 3); }//자식클래스에서 ss함수 재정의
+//
 //};
 //
 //int main()
 //{
 //	B bb;
-//	bb.setXY(1, 2);
-//	bb.showXY();
-//	C cc;
-//	cc.setXY(1, 3); //private상속이라 setXY()함수는 private취급이라 접근 불가.
-//
-//}
-//
-//class C :private B
-//{
-//
-//};
-////protected는 상속은 가능, 외부에서 접근 불가. 잘안씀
-//
-//
-////예시 - 상속에서 생성자와 소멸자
-//class A
-//{
-//public:
-//	A() { cout << "A의 생성자\n"; }
-//	~A() { cout << "A의 소멸자\n"; }
-//
-//};
-//class B :public A
-//{
-//public:
-//	B() { cout << "B의 생성자\n"; }
-//	~B() { cout << "B의 소멸자\n"; }
-//
-//};
-//
-//int main()
-//{
-//	B ob;
+//	A *pA = new A;
+//	A *pB = new B;
+//	cout << bb.dd(2) << endl;
+//	cout << pA->dd(2) << endl;
+//	cout << pB->dd(2) << endl;
+//	delete pA; delete pB;
 //	return 0;
 //}
-//
-////예시 - 자식 클래스 생성자에서 부모 클래스 생서앚에 매개변수 전달
-//class A
-//{
-//	int a;
-//public:
-//	A(int i)/* 여기 i로*/ {
-//		cout << "A의 생성자\n";
-//		a = i;
-//	}
-//	~A() { cout << "A의 소멸자\n"; }
-//	void showA() { cout << a << endl; }
-//
-//};
-//class B :public A {
-//	int b;
-//public:
-//	B(int i, int j) :A(i)// i는 기본클래스 생성자의 매개변수로 전달
-//	{cout << "B의 생성자\n"; }
-//};
-//
-//int main()
-//{
-//	B bb(10, 20);
-//	bb.showA();
-//	bb.showA();
-//	return 0;
-//}
-//
-////예시 - 계층적 다중상속에서 계층적 매개변수 전달
-//class B {
-//	double d; // double형 d 선언
-//public:
-//	B(double dd) { d = dd; } 
-//	double getD() { return d; }
-//};  //d와 연결된 함수
-//
-//class D1 :public B { //상속
-//	int i; //int형 i선언
-//public:
-//	D1(double dd, int ii) : B(dd) { i = ii; } //D2로부터 받은 매개변수를 전달
-//	int getI() { return i; }
-//};  //i와 연결된 함수
-//
-//class D2 :public D1 {
-//	char c;
-//public:
-//	D2(double dd, int ii, char cc) :D1(dd, ii) { c = cc; } //i와 d에 매개변수 전달
-//	void print() { 
-//		cout << "Double : " << getD() << endl; 
-//		cout << "int : " << getI() << endl;
-//		cout << "Char : " << c << endl;	
-//	}
-//};
-//
-//int main() {
-//	D2 d2(10.5, 10, 'H');
-//	cout << d2.getD() << '\t' << d2.getI() << endl;
-//	d2.print();
-//}
-//
-////예시 - Multiple inheritance
-//
-//class A1 
-//{
-//	int a;
-//public:
-//	A1() { cout << "A1's constructor.\n"; } //생성자 겹침
-//	~A1() {cout<< "A1's distroyer\n";}
-//	A1(int i) { a = i; }
-//	int getA1() { return a; }
-//
-//};
-//class A2
-//{
-//	int b;
-//public:
-//	A2() { cout << "A2's constructor.\n"; } //생성자 겹침
-//	~A2() { cout << "A2's distroyer\n"; }
-//	A2(int j) { b = j; }
-//	int getA2() { return b; }
-//};
-//class B : public A1, public A2 // Multiple inheritance
-//{
-//	int c;
-//public:
-//	B() { cout << "B's constructor.\n"; }
-//	~B() { cout << "B's distroyer\n"; }
-//	B(int i, int j) : A1(i), A2(j) {};
-//	void show() {
-//		cout << getA1() << endl << getA2() << endl;
-//	}
-//};
-//
-//int main() {
-//	B bb(1, 2);
-//	bb.show();
-//}
-//
-//
-////예시 - Using to Class
-//
-//class Name {
-//	string name;
-//public:
-//	Name(string s) { name = s; }
-//	void get_name(string s) { name = s; }
-//	void print_name() { cout << name << "의 전화번호는 "; }
-//
-//};
-//
-//class Phone :public Name {
-//	string phone;
-//public:
-//	Phone(string s, string p ) : Name(s){phone=p;};
-//	void get_phone(string p) { phone = p; }
-//	void pring_phone() 
-//	{
-//		print_name(); cout << phone;
-//	}
-//};
-//
-//int main() {
-//	Phone h("nope","010-010");
-//	
-//	h.pring_phone();
-//
-//}
-//// 복습하기... 어렵
 
-//사람클래스(Man,멤버변수:이름,나이)를 만드시오
-//멤버변수를 초기화하는 생성자를 구현하시오.
-//기타 함수를 구현하시오.
-//사람클래스로부터 상속받은 학생클래스(student)를 만드시오.
-//(멤버변수:반,학번), 생성자, 기타함수
-//사람클래스로부터 상속받은 교수(Teacher)클래스를 만드시오.
-//(멤버변수:전공,담당과목), 생성자, 기타함수
-//main함수에서 학생객체와 교수객체를 만들어 테스트해보시오.
+//pure virtual function 
 
-class Man {
-	const char* n;
-	int a;
+class Shape {
+protected:
+	double x, y;
 public:
-	const char* name;
-	int age;
-	Man(const char* nn, int aa) { n = nn, a = aa; }
-	
-	void setName(const char* name) //set함수에서 집어넣고
-	{
-		n=name;
-	}
-	void setAge(int age)
-	{
-		a = age;
-	}
-	const char* getName() //get함수에서 출력 [cin cout]아님
-	{
-		return n;
-	}
-	int getAge()
-	{
-		return a;
-	}
-	
-};
+	//virtual double area() { return 0; }// 일반 가상 함수
+	virtual double area() = 0;
 
-class student : public Man 
+};
+class Rectangle : public Shape
 {
-	int C;
-	int SN;
+private:
+	double height, width;
 public:
-	const char* Class;
-	int StudentNumber;
-	student(const char* nn, int aa, int CC, int SNSN) : Man(nn,aa) { C = CC, SN = SNSN; }
-
-	void setClass(int Class) //set함수에서 집어넣고
+	Rectangle(double h, double w)
 	{
-		C = Class;
+		height = h; width = w;
 	}
-	void setStudentNumber(int StudentNumber)
+	double area() 
 	{
-		SN = StudentNumber;
-	}
-	int getClass() //get함수에서 출력 [cin cout]아님
-	{
-		return C;
-	}
-	int getStudentNumber()
-	{
-		return SN;
-	}
-	void print() 
-	{
-		cout << "이름 : " << getName() << '\n' << "나이 : " << 
-		getAge() << '\n' << "학년 : " << getClass() << '\n' << "학번 : " << getStudentNumber() << endl;
-	}
-
-	
-};
-class teacher : public Man {
-	const char* m;
-	const char* s;
-public:
-	const char* major;
-	const char* subject;
-	teacher (const char* nn, int aa, const char* mm, const char* ss) : Man(nn, aa) { m = mm, s = ss; }
-
-	void setmajor(const char* major) //set함수에서 집어넣고
-	{
-		m = major;
-	}
-	void setsubject(const char* subject)
-	{
-		s = subject;
-	}
-	const char* getmajor() //get함수에서 출력 [cin cout]아님
-	{
-		return m;
-	}
-	const char* getsubject()
-	{
-		return s;
-	}
-	void print()
-	{
-		cout << "이름 : " << getName() << '\n' << "나이 : " << getAge() << '\n' 
-			<< "전공 : " << getmajor() << '\n' << "담당과목 : " << getsubject();
+		return(width * height);
 	}
 
 };
+class Triangle : public Shape
+{
+private:
+	double height, width;
+public:
+	Triangle (double h, double w)
+	{
+		height = h; width = w;
+	}
+	double area()
+	{
+		return(width * height/2.0);
+	}
 
+};
 int main() {
-	student hj("heejun", 24, 3, 60170320);
-	teacher han("smile han", 11, "computer software", "c++");
-	hj.print();
-	han.print();
+
+	//Shape ss; 순수가상함수가 있으면 추상클래스이기 때문에 객체 만들 수 없음
+	Shape *p; // 포인터객체는 가능
+	Rectangle nemo(10, 20);
+	Triangle semo(10, 20);
+	
+	p = &nemo;//자식의 주소를 부모 포인터에 대입
+	cout << "nemo's size:" << p->area() << endl;
+	p = &semo;
+	cout << "semo's size:" << p->area() << endl;
+
 }
